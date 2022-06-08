2bfeb2b78900ba45701a629a81ed2b531ec13228
FraneSpralja <spralja.frane@gmail.com>
2022-06-07 23:54:49 -0400

    desarrollo slide portada inicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,51 @@
 // VARIABLES
+const pageHero = document.querySelector('#page-hero');
+
+
 
 // EVENT LISTENERS
+eventListener()
 
-// FUNCIONES
+function eventListener () {
+    
+    document.addEventListener('DOMContentLoaded', () => {
+
+        pageHero.classList.add('slide-uno')
+
+        sliderHero()
+    });
+
+}
+
+function sliderHero(){
+    
+    const slider = () => {setTimeout(function newSlide() {
+        slideDos();
+
+        setTimeout(() => {
+                slideTres();
+
+                setTimeout(() => {
+                    slideUno();
+                }, 3000)
+            }, 3000)
+        }, 3000)}
+        
+
+    function slideDos() {
+        pageHero.classList.remove('slide-uno');
+        pageHero.classList.add('slide-dos');
+    }
+
+    function slideTres() {
+        pageHero.classList.remove('slide-dos');
+        pageHero.classList.add('slide-tres');
+    }
+
+    function slideUno () {
+        pageHero.classList.remove('slide-tres');
+        pageHero.classList.add('slide-uno');
+    }
+
+    setInterval(slider, 9000)
+}
